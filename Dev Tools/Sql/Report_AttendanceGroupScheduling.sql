2052c65a4148a552e36f9bc1ab2ac56d1767deec
Mike Peterson <mike@sparkabilitygroup.com>
2020-07-02 16:47:19 -0700

    - Update Populate_Attendance to an option to populate Group Scheduling assignments

--- Dev Tools/Sql/Report_AttendanceGroupScheduling.sql
+++ Dev Tools/Sql/Report_AttendanceGroupScheduling.sql
@@ -1,9 +1,22 @@
-select p.NickName, p.LastName, a.ScheduledToAttend, a.RequestedToAttend, ao.OccurrenceDate, g.Name [Group.Name], l.Name [Location.Name], s.Name [Schedule.Name]
-from Attendance a
-join AttendanceOccurrence ao on a.OccurrenceId = ao.Id
-join [Group] g on ao.GroupId = g.Id
-join PersonAlias pa on a.PersonAliasId = pa.Id
-join Person p on pa.PersonId = p.Id
-join [Location] l on ao.LocationId = l.Id
-join Schedule s on ao.ScheduleId= s.Id
-where a.ScheduledToAttend is not null
+SELECT pa.PersonId, p.NickName, p.LastName, a.RSVP, a.ScheduledToAttend, a.RequestedToAttend, ao.Id [Occurrence.Id], ao.OccurrenceDate, g.Name [Group.Name], ao.SundayDate, l.Name [Location.Name], s.Name [Schedule.Name]
+FROM Attendance a
+JOIN AttendanceOccurrence ao
+	ON a.OccurrenceId = ao.Id
+JOIN [Group] g
+	ON ao.GroupId = g.Id
+JOIN PersonAlias pa
+	ON a.PersonAliasId = pa.Id
+JOIN Person p
+	ON pa.PersonId = p.Id
+JOIN [Location] l
+	ON ao.LocationId = l.Id
+JOIN Schedule s
+	ON ao.ScheduleId = s.Id
+WHERE (
+		(a.ScheduledToAttend = 1) -- Accepted
+		--or (RequestedToAttend = 1 and RSVP in (1))  -- Pending
+		--or (RequestedToAttend = 1 and RSVP in (0))  -- Declined
+		) AND ao.SundayDate > GetDate() AND ao.SundayDate < DATEADD(day, 7, GetDate())
+	--and g.Name = 'Children''s'
+	AND s.IsActive = 1
+ORDER BY ao.SundayDate, ao.OccurrenceDate, g.Name, l.Name, s.Name
